bb961782e1c3a3e46f178818db00225d3c7acf2e
Brendon Butler <bbutler452@gmail.com>
2016-03-23 20:27:01 -0400

    Added console input

--- src/net/sparkzz/visionless/Visionless.java
+++ src/net/sparkzz/visionless/Visionless.java
@@ -1,13 +1,24 @@
 package net.sparkzz.visionless;
 
+import java.io.IOException;
+
 /**
  * Created by Brendon Butler on 3/23/2016.
  */
 public class Visionless {
 
+    private static String input;
+
     public static void main(String[] args) {
         Game game = new Game();
 
-        game.post("test");
+        try {
+            game.post("What will you do?");
+            input = game.prompt();
+
+            game.post(input);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
