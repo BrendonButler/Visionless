7dc99634f58a38fbd8c277583485e4c2eda1762b
Brendon Butler <bbutler452@gmail.com>
2016-05-11 22:06:32 -0400

    Started using ModestAPI as the game Engine

--- src/net/sparkzz/visionless/Visionless.java
+++ src/net/sparkzz/visionless/Visionless.java
@@ -1,39 +1,23 @@
 package net.sparkzz.visionless;
 
-import java.io.IOException;
+import net.sparkzz.modest.ModestGame;
+import net.sparkzz.modest.io.console.Console;
 
 /**
  * Created by Brendon Butler on 3/23/2016.
  */
-public class Visionless {
+public class Visionless extends ModestGame {
 
-    private static boolean running = false;
-    private static Game game;
-    private static String input;
+	public Visionless() {
+		run();
+	}
 
     public static void main(String[] args) {
-        game = new Game();
-
-        running = true;
-
-        try {
-            run();
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+		new Visionless();
     }
 
-    // game loop
-    private static void run() throws IOException {
-        while (running) {
-            game.post("What will you do?");
-            input = game.prompt();
-            game.post(input);
-
-            if (input.equalsIgnoreCase("quit")) {
-                game.post("Thanks for playing!");
-                running = false;
-            }
-        }
-    }
+	@Override
+	public void postInit() {
+		Console.out("Working!");
+	}
 }
